88b06aba4fdca0ebbb5d520a237de759c13dccfd
danzjamz <djl.hovley@gmail.com>
2020-03-10 11:21:16 -0600

    addded post route

--- app.py
+++ app.py
@@ -30,13 +30,26 @@ class TodoSchema(ma.Schema):
 todo_schema = TodoSchema()
 todos_schema = TodoSchema(many=True)
 
-    
-
 
 
 @app.route('/')
 def hello_world():
     return 'hello world!'
+
+@app.route('/todo', methods=["POST"])
+def add_todo():
+    title = request.json['title']
+    done = request.json['done']
+
+    new_todo = Todo(title, done)
+
+    db.session.add(new_todo)
+    db.session.commit()
+
+    todo = Todo.query.get(new_todo.id)
+    return todo_schema.jsonify(todo)
+
+
 
 if __name__ == '__main__':
     app.debug = True
